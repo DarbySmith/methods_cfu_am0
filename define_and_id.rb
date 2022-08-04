4b95325ed2c4863d73e7ac7edc44c458ed6ab0ad
Darby Smith <newman.darby2@gmail.com>
2022-08-04 15:09:26 -0400

    Add explanations

--- define_and_id.rb
+++ define_and_id.rb
@@ -5,23 +5,21 @@
 # The downcase method is called on the string object "Hello World"
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
-"Hello World".downcase
+p "Hello World".downcase
 
 # YOU DO the rest:
 
+# The return value is true because the variable contains "Hello"
+p "Hello World".include?("Hello")
 
-"Hello World".include?("Hello")
+# The return value is false because the variable does not end with "Hello"
+p "Hello World".end_with?("Hello")
 
+# The return value is true because the variable ends with "rld"
+p "Hello World".end_with?("rld")
 
-"Hello World".end_with?("Hello")
+# The return value is true because the variable is an even number
+p 12.even?
 
-
-"Hello World".end_with?("rld")
-
-
-12.even?
-
-
-18.next
-
-
+# The return value is "19" because the method prints the next integer 
+p 18.next
